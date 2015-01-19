2a49245ddc8a03b616a43803bdc1290b05134495
Anis <anishireche@gmail.com>
2015-01-19 18:06:44 +0100

    Update renderPrePassMgr.cpp

--- Engine/source/renderInstance/renderPrePassMgr.cpp
+++ Engine/source/renderInstance/renderPrePassMgr.cpp
@@ -1107,7 +1107,7 @@ void RenderPrePassMgr::_initShaders()
 void RenderPrePassMgr::clearBuffers()
 {
    // Clear z-buffer.
-   GFX->clear( GFXClearZBuffer | GFXClearStencil, ColorI::BLACK, 1.0f, 0);
+   GFX->clear( GFXClearTarget | GFXClearZBuffer | GFXClearStencil, ColorI::BLACK, 1.0f, 0);
 
    if ( !mClearGBufferShader )
       return;
