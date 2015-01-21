a19713974b51f937d3401e24f80344714df00f89
Azaezel <quillus@hotmail.com>
2015-01-21 05:31:51 -0600

    test swatches

---
+++ Templates/Full/game/art/shapes/textures/materials.cs
@@ -0,0 +1,39 @@
+
+singleton Material(PBRTEST_PBRstone)
+{
+   mapTo = "PBRstone";
+   diffuseColor[0] = "1 1 1 1";
+   roughness[0] = "1";
+   metalness[0] = "50";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/textures/Stone_A.dds";
+   normalMap[0] = "art/shapes/textures/Stone_N.png";
+   specularMap[0] = "art/shapes/textures/Stone_S.dds";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(PBRTEST_PBRMetal)
+{
+   mapTo = "PBRMetal";
+   diffuseColor[0] = "1 1 1 1";
+   roughness[0] = "1";
+   metalness[0] = "50";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/textures/Metal_A.dds";
+   normalMap[0] = "art/shapes/textures/Metal_N.png";
+   specularMap[0] = "art/shapes/textures/Metal_S.dds";
+   materialTag0 = "Miscellaneous";
+};
+
+singleton Material(PBRTEST_PBRWood)
+{
+   mapTo = "PBRWood";
+   diffuseColor[0] = "1 1 1 1";
+   roughness[0] = "1";
+   metalness[0] = "50";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/textures/Wood1_A.dds";
+   normalMap[0] = "art/shapes/textures/Wood1_N.png";
+   specularMap[0] = "art/shapes/textures/Wood1_S.dds";
+   materialTag0 = "Miscellaneous";
+};
