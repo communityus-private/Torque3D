fb1c0f9a488fecd48a9b693d7aeded63193d3a9b
Azaezel <quillus@hotmail.com>
2015-03-20 14:13:03 -0500

    cleanup

--- Templates/Full/game/shaders/common/offlineLPVReflectP.hlsl
+++ Templates/Full/game/shaders/common/offlineLPVReflectP.hlsl
@@ -72,7 +72,7 @@ float4 main( Conn IN ) : COLOR0
             break; 
        }
 
-       float3 color = tex3Dlod(lpvData, float4(volume_position,1.1+matInfoSample.b/8)).rgb;
+       float3 color = tex3Dlod(lpvData, float4(volume_position,1.1+matInfoSample.b)).rgb;
        if ( length(color) > 0.0 )
        {
             final_color += color;
