abdec98974379689253716cc68c2c09af71df070
Azaezel <quillus@hotmail.com>
2015-03-22 15:22:52 -0500

    refactor - use preexisting methodology to get color at point.

--- Engine/source/T3D/offlineLPV/offlineLPV.cpp
+++ Engine/source/T3D/offlineLPV/offlineLPV.cpp
@@ -951,12 +951,7 @@ void OfflineLPV::regenVolume()
                                        Resource<GBitmap> diffuseTex = GBitmap::load(mat->mDiffuseMapFilename[0]);
                                        if (diffuseTex != NULL)
                                        {
-                                          U32 w = diffuseTex->getWidth();
-                                          U32 h = diffuseTex->getHeight();
-
-                                          ColorI result;
-                                          diffuseTex->getColor(uv.x * w, uv.y * h, result);
-                                          voxel_color = result;
+                                          voxel_color = diffuseTex->sampleTexel(uv.x, uv.y);
                                        } else {
                                           voxel_color = mat->mDiffuse[0];
                                        }
@@ -999,12 +994,7 @@ void OfflineLPV::regenVolume()
                                           Resource<GBitmap> diffuseTex = GBitmap::load(mat->mDiffuseMapFilename[0]);
                                           if (diffuseTex != NULL)
                                           {
-                                             U32 w = diffuseTex->getWidth();
-                                             U32 h = diffuseTex->getHeight();
-
-                                             ColorI result;
-                                             diffuseTex->getColor(uv.x * w, uv.y * h, result);
-                                             voxel_color = result;
+                                             voxel_color = diffuseTex->sampleTexel(uv.x, uv.y);
                                           } else {
                                              voxel_color = mat->mDiffuse[0];
                                           }
@@ -1058,12 +1048,7 @@ void OfflineLPV::regenVolume()
                                                 Resource<GBitmap> diffuseTex = GBitmap::load(mat->mDiffuseMapFilename[0]);
                                                 if (diffuseTex != NULL)
                                                 {
-                                                   U32 w = diffuseTex->getWidth();
-                                                   U32 h = diffuseTex->getHeight();
-
-                                                   ColorI result;
-                                                   diffuseTex->getColor(uv.x * w, uv.y * h, result);
-                                                   voxel_color = result;
+                                                   voxel_color = diffuseTex->sampleTexel(uv.x, uv.y);
                                                 } else {
                                                    voxel_color = mat->mDiffuse[0];
                                                 }
