89a7eb650e57bd8b945922902e3c69c108ac653c
Azaezel <quillus@hotmail.com>
2015-10-23 16:25:47 -0500

    MRT references

--- Templates/Full/game/shaders/common/lighting/advanced/deferredClearGBufferP.hlsl
+++ Templates/Full/game/shaders/common/lighting/advanced/deferredClearGBufferP.hlsl
@@ -20,11 +20,12 @@
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
 
+#include "../../postfx/postFx.hlsl"
 struct Fragout
 {
-   float4 col : COLOR0;
-   float4 col1 : COLOR1;
-   float4 col2 : COLOR2;
+   float4 col : TORQUE_TARGET0;
+   float4 col1 : TORQUE_TARGET1;
+   float4 col2 : TORQUE_TARGET2;
 };
 
 //-----------------------------------------------------------------------------
