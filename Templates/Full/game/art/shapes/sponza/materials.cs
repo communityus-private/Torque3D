a576d65eecce08beafee0d562ee41e8d75233b50
Areloch <Areloch@gmail.com>
2017-04-13 19:23:35 -0500

    Sponza!

---
+++ Templates/Full/game/art/shapes/sponza/materials.cs
@@ -0,0 +1,327 @@
+
+singleton Material(sponza_Material__298)
+{
+   mapTo = "Material__298";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Background_Albedo.DDS";
+   normalMap[0] = "art/shapes/sponza/Background_Normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Background_Roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_Material__57)
+{
+   mapTo = "Material__57";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/VasePlant_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/VasePlant_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/VasePlant_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+   alphaTest = "1";
+   alphaRef = "255";
+};
+
+singleton Material(sponza_vase_round)
+{
+   mapTo = "vase_round";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/VaseRound_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/VaseRound_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/VaseRound_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_leaf)
+{
+   mapTo = "leaf";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Thorn_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Thorn_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Thorn_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+   alphaTest = "1";
+   alphaRef = "64";
+};
+
+singleton Material(sponza__6___Default)
+{
+   mapTo = "_6___Default";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Fabric_Red_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Fabric_Red_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Fabric_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Fabric_metallic.DDS";
+};
+
+singleton Material(sponza_column_b)
+{
+   mapTo = "column_b";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Column_b_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Column_b_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Column_b_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_arch)
+{
+   mapTo = "arch";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Arch_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Arch_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Arch_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_column_c)
+{
+   mapTo = "column_c";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseColor[1] = "White";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Column_c_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Column_c_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Column_c_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_chain)
+{
+   mapTo = "chain";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/ChainTexture_Albedo.dds";
+   normalMap[0] = "art/shapes/sponza/ChainTexture_Normal.dds";
+   roughMap[0] = "art/shapes/sponza/ChainTexture_Roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/ChainTexture_Metallic.dds";
+   alphaTest = "1";
+   alphaRef = "255";
+};
+
+singleton Material(sponza_Material__47)
+{
+   mapTo = "Material__47";
+   diffuseColor[0] = "0.588 0.588 0.588 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sponza_ceiling)
+{
+   mapTo = "ceiling";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Ceiling_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Ceiling_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Ceiling_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_Material__25)
+{
+   mapTo = "Material__25";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Lion_Albedo.DDS";
+   normalMap[0] = "art/shapes/sponza/Lion_Normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Lion_Roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_fabric_a)
+{
+   mapTo = "fabric_a";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Fabric_Green_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Fabric_Blue_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Fabric_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Fabric_metallic.DDS";
+};
+
+singleton Material(sponza_vase)
+{
+   mapTo = "vase";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Vase_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Vase_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Vase_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_fabric_g)
+{
+   mapTo = "fabric_g";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Curtain_Green_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Curtain_Green_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Curtain_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Curtain_metallic.DDS";
+};
+
+singleton Material(sponza_fabric_a)
+{
+   mapTo = "fabric_a";
+   diffuseColor[0] = "0.588 0.588 0.588 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sponza_fabric_d)
+{
+   mapTo = "fabric_d";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Fabric_Red_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Fabric_Red_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Fabric_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Fabric_metallic.DDS";
+};
+
+singleton Material(sponza_details)
+{
+   mapTo = "details";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Details_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Details_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Details_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+   invertSmoothness[0] = "0";
+};
+
+singleton Material(sponza_fabric_d)
+{
+   mapTo = "fabric_d";
+   diffuseColor[0] = "0.588 0.588 0.588 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sponza_fabric_e)
+{
+   mapTo = "fabric_e";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Fabric_Blue_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Curtain_Blue_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Fabric_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Fabric_metallic.DDS";
+};
+
+singleton Material(sponza_details)
+{
+   mapTo = "details";
+   diffuseColor[0] = "0.588 0.588 0.588 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sponza_floor)
+{
+   mapTo = "floor";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Floor_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Floor_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Floor_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+};
+
+singleton Material(sponza_fabric_f)
+{
+   mapTo = "fabric_f";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Curtain_Red_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Curtain_Blue_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Curtain_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Curtain_metallic.DDS";
+};
+
+singleton Material(sponza_roof)
+{
+   mapTo = "roof";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Roof_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Roof_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Roof_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Dielectric_metallic.DDS";
+   invertSmoothness[0] = "1";
+};
+
+singleton Material(sponza_fabric_f)
+{
+   mapTo = "fabric_f";
+   diffuseColor[0] = "0.588 0.588 0.588 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+};
+
+singleton Material(sponza_fabric_c)
+{
+   mapTo = "fabric_c";
+   diffuseColor[0] = "1 0.991393 0.991393 1";
+   smoothness[0] = "1";
+   metalness[0] = "6";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/sponza/Sponza_Curtain_Blue_diffuse.DDS";
+   normalMap[0] = "art/shapes/sponza/Sponza_Fabric_Blue_normal.DDS";
+   roughMap[0] = "art/shapes/sponza/Sponza_Fabric_roughness.DDS";
+   metalMap[0] = "art/shapes/sponza/Sponza_Fabric_metallic.DDS";
+};
