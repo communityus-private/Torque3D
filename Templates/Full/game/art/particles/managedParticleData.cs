38131e90e7c455e32fb0868100bd1ece403a0703
Azaezel <quillus@hotmail.com>
2017-04-17 22:31:22 -0500

    vase fires/lights prefab

--- Templates/Full/game/art/particles/managedParticleData.cs
+++ Templates/Full/game/art/particles/managedParticleData.cs
@@ -23,3 +23,26 @@
 // This is the default save location for any Particle datablocks created in the
 // Particle Editor (this script is executed from onServerCreated())
 
+
+datablock ParticleData(fireplaceP : DefaultParticle)
+{
+   animateTexture = "1";
+   animTexTiling = "8 5";
+   animTexFrames = "0-40";
+   textureName = "art/particles/fireplace.png";
+   animTexName = "art/particles/fireplace.png";
+   dragCoefficient = "0";
+   inheritedVelFactor = "0";
+   lifetimeMS = "1000";
+   lifetimeVarianceMS = "0";
+   spinRandomMin = "0";
+   spinRandomMax = "1";
+   framesPerSec = "40";
+   spinSpeed = "0";
+   colors[0] = "1 1 1 1";
+   colors[1] = "1 1 1 1";
+   sizes[0] = "2";
+   sizes[1] = "2";
+   times[1] = "1";
+   times[2] = "1";
+};
