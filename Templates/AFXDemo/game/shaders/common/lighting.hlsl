77365efd7406605fa79cdeed036864ef71c6ea14
Azaezel <quillus@hotmail.com>
2019-04-02 10:06:01 -0500

    shaders synced to full template

--- Templates/AFXDemo/game/shaders/common/lighting.hlsl
+++ Templates/AFXDemo/game/shaders/common/lighting.hlsl
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2012 GarageGames, LLC
-//
+// Portions Copyright Zefiros
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
 // deal in the Software without restriction, including without limitation the
@@ -21,6 +21,8 @@
 //-----------------------------------------------------------------------------
 
 #include "./torque.hlsl"
+#include "./brdf.hlsl"
+#include "./shaderModelAutoGen.hlsl"
 
 #ifndef TORQUE_SHADERGEN
 
@@ -37,13 +39,30 @@ uniform float4 inLightColor[4];
 #endif
 
 uniform float4 ambient;
-#define ambientCameraFactor 0.3
-uniform float specularPower;
-uniform float4 specularColor;
+uniform float smoothness;
+uniform float metalness;
+uniform float4 albedo;
 
 #endif // !TORQUE_SHADERGEN
 
+inline float3 getDistanceVectorToPlane( float3 origin, float3 direction, float4 plane )
+{
+   float denum = dot( plane.xyz, direction.xyz );
+   float num = dot( plane, float4( origin, 1.0 ) );
+   float t = -num / denum;
 
+   return direction.xyz * t;
+}
+
+inline float3 getDistanceVectorToPlane( float negFarPlaneDotEye, float3 direction, float4 plane )
+{
+   float denum = dot( plane.xyz, direction.xyz );
+   float t = negFarPlaneDotEye / denum;
+
+   return direction.xyz * t;
+}
+
+//TODO fix compute 4 lights
 void compute4Lights( float3 wsView, 
                      float3 wsPosition, 
                      float3 wsNormal,
@@ -57,193 +76,164 @@ void compute4Lights( float3 wsView,
                         float4 inLightSpotDir[3],
                         float4 inLightSpotAngle,
                         float4 inLightSpotFalloff,
-                        float specularPower,
-                        float4 specularColor,
+                        float smoothness,
+                        float metalness,
+                        float4 albedo,
 
                      #endif // TORQUE_SHADERGEN
                      
                      out float4 outDiffuse,
                      out float4 outSpecular )
 {
-   // NOTE: The light positions and spotlight directions
-   // are stored in SoA order, so inLightPos[0] is the
-   // x coord for all 4 lights... inLightPos[1] is y... etc.
-   //
-   // This is the key to fully utilizing the vector units and
-   // saving a huge amount of instructions.
-   //
-   // For example this change saved more than 10 instructions 
-   // over a simple for loop for each light.
-   
-   int i;
 
-   float4 lightVectors[3];
-   for ( i = 0; i < 3; i++ )
-      lightVectors[i] = wsPosition[i] - inLightPos[i];
-
-   float4 squareDists = 0;
-   for ( i = 0; i < 3; i++ )
-      squareDists += lightVectors[i] * lightVectors[i];
-
-   // Accumulate the dot product between the light 
-   // vector and the normal.
-   //
-   // The normal is negated because it faces away from
-   // the surface and the light faces towards the
-   // surface... this keeps us from needing to flip
-   // the light vector direction which complicates
-   // the spot light calculations.
-   //
-   // We normalize the result a little later.
-   //
-   float4 nDotL = 0;
-   for ( i = 0; i < 3; i++ )
-      nDotL += lightVectors[i] * -wsNormal[i];
-
-   float4 rDotL = 0;
-   #ifndef TORQUE_BL_NOSPECULAR
-
-      // We're using the Phong specular reflection model
-      // here where traditionally Torque has used Blinn-Phong
-      // which has proven to be more accurate to real materials.
-      //
-      // We do so because its cheaper as do not need to 
-      // calculate the half angle for all 4 lights.
-      //   
-      // Advanced Lighting still uses Blinn-Phong, but the
-      // specular reconstruction it does looks fairly similar
-      // to this.
-      //
-      float3 R = reflect( wsView, -wsNormal );
-
-      for ( i = 0; i < 3; i++ )
-         rDotL += lightVectors[i] * R[i];
-
-   #endif
- 
-   // Normalize the dots.
-   //
-   // Notice we're using the half type here to get a
-   // much faster sqrt via the rsq_pp instruction at 
-   // the loss of some precision.
-   //
-   // Unless we have some extremely large point lights
-   // i don't believe the precision loss will matter.
-   //
-   half4 correction = (half4)rsqrt( squareDists );
-   nDotL = saturate( nDotL * correction );
-   rDotL = clamp( rDotL * correction, 0.00001, 1.0 );
-
-   // First calculate a simple point light linear 
-   // attenuation factor.
-   //
-   // If this is a directional light the inverse
-   // radius should be greater than the distance
-   // causing the attenuation to have no affect.
-   //
-   float4 atten = saturate( 1.0 - ( squareDists * inLightInvRadiusSq ) );
-
-   #ifndef TORQUE_BL_NOSPOTLIGHT
-
-      // The spotlight attenuation factor.  This is really
-      // fast for what it does... 6 instructions for 4 spots.
-
-      float4 spotAtten = 0;
-      for ( i = 0; i < 3; i++ )
-         spotAtten += lightVectors[i] * inLightSpotDir[i];
-
-      float4 cosAngle = ( spotAtten * correction ) - inLightSpotAngle;
-      atten *= saturate( cosAngle * inLightSpotFalloff );
-
-   #endif
-
-   // Finally apply the shadow masking on the attenuation.
-   atten *= shadowMask;
-
-   // Get the final light intensity.
-   float4 intensity = nDotL * atten;
-
-   // Combine the light colors for output.
-   outDiffuse = 0;
-   for ( i = 0; i < 4; i++ )
-      outDiffuse += intensity[i] * inLightColor[i];
-
-   // Output the specular power.
-   float4 specularIntensity = pow( rDotL, specularPower.xxxx ) * atten;
-   
-   // Apply the per-light specular attenuation.
-   float4 specular = float4(0,0,0,1);
-   for ( i = 0; i < 4; i++ )
-      specular += float4( inLightColor[i].rgb * inLightColor[i].a * specularIntensity[i], 1 );
-
-   // Add the final specular intensity values together
-   // using a single dot product operation then get the
-   // final specular lighting color.
-   outSpecular = specularColor * specular;
+   outDiffuse = float4(0,0,0,0);
+   outSpecular = float4(0,0,0,0);
 }
 
-
-// This value is used in AL as a constant power to raise specular values
-// to, before storing them into the light info buffer. The per-material 
-// specular value is then computer by using the integer identity of 
-// exponentiation: 
-//
-//    (a^m)^n = a^(m*n)
-//
-//       or
-//
-//    (specular^constSpecular)^(matSpecular/constSpecular) = specular^(matSpecular*constSpecular)   
-//
-#define AL_ConstantSpecularPower 12.0f
-
-/// The specular calculation used in Advanced Lighting.
-///
-///   @param toLight    Normalized vector representing direction from the pixel 
-///                     being lit, to the light source, in world space.
-///
-///   @param normal  Normalized surface normal.
-///   
-///   @param toEye   The normalized vector representing direction from the pixel 
-///                  being lit to the camera.
-///
-float AL_CalcSpecular( float3 toLight, float3 normal, float3 toEye )
+struct Surface
 {
-   // (R.V)^c
-   float specVal = dot( normalize( -reflect( toLight, normal ) ), toEye );
+	float3 P;				// world space position
+	float3 N;				// world space normal
+	float3 V;				// world space view vector
+	float4 baseColor;		// base color [0 -> 1] (rgba)
+	float metalness;		// metalness [0:dielectric -> 1:metal]
+	float roughness;		// roughness: [0:smooth -> 1:rough] (linear)
+	float roughness_brdf; // roughness remapped from linear to BRDF
+   float depth;         // depth: [0:near -> 1:far] (linear)
+   float ao;            // ambient occlusion [0 -> 1]
+   float matFlag;       // material flag - use getFlag to retreive 
 
-   // Return the specular factor.
-   return pow( max( specVal, 0.00001f ), AL_ConstantSpecularPower );
-}
+	float NdotV;			// cos(angle between normal and view vector)
+	float3 f0;				// fresnel value (rgb)
+	float3 albedo;			// diffuse light absorbtion value (rgb)
+	float3 R;				// reflection vector
+	float3 F;				// fresnel term computed from f0, N and V
 
-/// The output for Deferred Lighting
-///
-///   @param toLight    Normalized vector representing direction from the pixel 
-///                     being lit, to the light source, in world space.
-///
-///   @param normal  Normalized surface normal.
-///   
-///   @param toEye   The normalized vector representing direction from the pixel 
-///                  being lit to the camera.
-///
-float4 AL_DeferredOutput(
-      float3   lightColor,
-      float3   diffuseColor,
-      float4   matInfo,
-      float4   ambient,
-      float    specular,
-      float    shadowAttenuation)
+	inline void Update()
+	{
+		NdotV = abs(dot(N, V)) + 1e-5f; // avoid artifact
+
+		albedo = baseColor.rgb * (1.0 - metalness);
+		f0 = lerp(0.04.xxx, baseColor.rgb, metalness);
+
+		R = -reflect(V, N);
+		float f90 = saturate(50.0 * dot(f0, 0.33));
+		F = F_Schlick(f0, f90, NdotV);
+	}
+};
+
+inline Surface createSurface(float4 gbuffer0, TORQUE_SAMPLER2D(gbufferTex1), TORQUE_SAMPLER2D(gbufferTex2), in float2 uv, in float3 wsEyePos, in float3 wsEyeRay, in float4x4 invView)
 {
-   float3 specularColor = float3(specular, specular, specular);
-   bool metalness = getFlag(matInfo.r, 3);
-   if ( metalness )
-   {
-       specularColor = 0.04 * (1 - specular) + diffuseColor * specular;
-   }
-   
-   //specular = color * map * spec^gloss
-   float specularOut = (specularColor * matInfo.b * min(pow(abs(specular), max(( matInfo.a/ AL_ConstantSpecularPower),1.0f)),matInfo.a)).r;
-   
-   lightColor *= shadowAttenuation;
-   lightColor += ambient.rgb;
-   return float4(lightColor.rgb, specularOut); 
+	Surface surface = (Surface)0;
+
+   float4 gbuffer1 = TORQUE_TEX2DLOD(gbufferTex1, float4(uv,0,0));
+   float4 gbuffer2 = TORQUE_TEX2DLOD(gbufferTex2, float4(uv,0,0));
+
+   surface.depth = gbuffer0.a;
+	surface.P = wsEyePos + wsEyeRay * surface.depth;
+	surface.N = mul(invView, float4(gbuffer0.xyz,0)).xyz; //TODO move t3d to use WS normals
+	surface.V = normalize(wsEyePos - surface.P);
+	surface.baseColor = gbuffer1;
+   const float minRoughness=1e-4;
+	surface.roughness = clamp(1.0 - gbuffer2.b, minRoughness, 1.0); //t3d uses smoothness, so we convert to roughness.
+	surface.roughness_brdf = surface.roughness * surface.roughness;
+	surface.metalness = gbuffer2.a;
+   surface.ao = gbuffer2.g;
+   surface.matFlag = gbuffer2.r;
+
+	surface.Update();
+	return surface;
 }
+
+struct SurfaceToLight
+{
+	float3 L;				// surface to light vector
+   float3 Lu;				// un-normalized surface to light vector
+	float3 H;				// half-vector between view vector and light vector
+	float NdotL;			// cos(angle between N and L)
+	float HdotV;			// cos(angle between H and V) = HdotL = cos(angle between H and L)
+	float NdotH;			// cos(angle between N and H)
+};
+
+inline SurfaceToLight createSurfaceToLight(in Surface surface, in float3 L)
+{
+	SurfaceToLight surfaceToLight = (SurfaceToLight)0;
+   surfaceToLight.Lu = L;
+	surfaceToLight.L = normalize(L);
+	surfaceToLight.H = normalize(surface.V + surfaceToLight.L);
+	surfaceToLight.NdotL = saturate(dot(surfaceToLight.L, surface.N));
+	surfaceToLight.HdotV = saturate(dot(surfaceToLight.H, surface.V));
+	surfaceToLight.NdotH = saturate(dot(surfaceToLight.H, surface.N));
+
+	return surfaceToLight;
+}
+
+float3 BRDF_GetSpecular(in Surface surface, in SurfaceToLight surfaceToLight)
+{
+	float f90 = saturate(50.0 * dot(surface.f0, 0.33));
+	float3 F = F_Schlick(surface.f0, f90, surfaceToLight.HdotV);
+	float Vis = V_SmithGGXCorrelated(surface.NdotV, surfaceToLight.NdotL, surface.roughness_brdf);
+	float D = D_GGX(surfaceToLight.NdotH, surface.roughness_brdf);
+	float3 Fr = D * F * Vis / M_PI_F;
+	return Fr;
+}
+
+float3 BRDF_GetDiffuse(in Surface surface, in SurfaceToLight surfaceToLight)
+{
+   //getting some banding with disney method, using lambert instead - todo futher testing
+	float Fd = 1.0 / M_PI_F;
+   //energy conservation - remove this if reverting back to disney method
+   float3 kD = 1.0.xxx - surface.F;
+	kD *= 1.0 - surface.metalness;
+   float3 diffuse = kD * surface.baseColor.rgb * Fd;
+	return diffuse;
+}
+
+//attenuations functions from "moving frostbite to pbr paper"
+//https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
+float smoothDistanceAtt ( float squaredDistance , float invSqrAttRadius )
+{
+   float factor = squaredDistance * invSqrAttRadius ;
+   float smoothFactor = saturate (1.0f - factor * factor );
+   return sqr(smoothFactor);
+}
+
+float getDistanceAtt( float3 unormalizedLightVector , float invSqrAttRadius )
+{
+   float sqrDist = dot ( unormalizedLightVector , unormalizedLightVector );
+   float attenuation = 1.0 / (max ( sqrDist , 0.01*0.01) );
+   attenuation *= smoothDistanceAtt ( sqrDist , invSqrAttRadius );
+   return attenuation;
+}
+
+ float getSpotAngleAtt( float3 normalizedLightVector , float3 lightDir , float2 lightSpotParams )
+ {
+   float cd = dot ( lightDir , normalizedLightVector );
+   float attenuation = saturate ( ( cd - lightSpotParams.x ) / lightSpotParams.y );
+   // smooth the transition
+   return sqr(attenuation);
+}
+
+inline float3 getDirectionalLight(in Surface surface, in SurfaceToLight surfaceToLight, float3 lightColor, float lightIntensity, float shadow)
+{
+   float3 factor = lightColor * max(surfaceToLight.NdotL, 0) * shadow * lightIntensity;
+   float3 diffuse = BRDF_GetDiffuse(surface,surfaceToLight) * factor;
+   float3 spec = BRDF_GetSpecular(surface,surfaceToLight) * factor;
+
+   float3 final = max(0.0f, diffuse + spec * surface.ao);
+   return final;
+}
+
+inline float3 getPunctualLight(in Surface surface, in SurfaceToLight surfaceToLight, float3 lightColor, float lightIntensity, float radius, float shadow)
+{
+   float attenuation = getDistanceAtt(surfaceToLight.Lu, radius);
+   float3 factor = lightColor * max(surfaceToLight.NdotL, 0) * shadow * lightIntensity * attenuation;
+
+   float3 diffuse = BRDF_GetDiffuse(surface,surfaceToLight) * factor;
+   float3 spec = BRDF_GetSpecular(surface,surfaceToLight) * factor;
+
+   float3 final = max(0.0f, diffuse + spec * surface.ao * surface.F);
+   return final;
+}
+
+//Probe IBL stuff
